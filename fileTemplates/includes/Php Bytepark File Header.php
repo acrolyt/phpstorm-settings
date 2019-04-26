0aa1c7473633945ba43d9cfc2f85abe8c50891c9
Marco <acrolyt@gmail.com>
2019-04-26 11:25:24 +0200

    PS-191.7141.5 <acrolyt@Acrolyt-MBP.local Update baseRefactoring.xml, ide.general.xml, PHP.xml, project.default.xml, diff.xml, KeyPromoterXStatistic.xml, K1.xml, find.xml, ui.lnf.xml, dataSources.xml, filetypes.xml, php.xml, Marco Mac.xml, maw.icls, vcs.xml, code.style.schemes.xml, databaseSettings.xml, notifications.xml, path.macros.xml, databaseDrivers.xml, editor.xml, usageView.xml	Create Electric Expresso.icls, ObsidianPlus.icls, Jellybean inspired.icls, Seti-UI.icls, Sunburst.icls, Norway Today by Erik Kubica_0.icls, Zeus_0.icls, Monokai_Sublime.icls, Visual Studio 2013.icls, Retta.icls, PHP Field Doc Comment.php, dark_blue_sea.icls, Php Bytepark File Header.php, updates.xml

---
+++ fileTemplates/includes/Php Bytepark File Header.php
@@ -0,0 +1,10 @@
+/**
+ * File of ${NAME} class
+ *
+ * PHP version 5.6
+ *
+ * @author     bytepark GmbH <code@bytepark.de>
+ * @copyright  ${YEAR} - bytepark GmbH
+ * @license    https://opensource.org/licenses/MIT MIT
+ * @link       http://www.bytepark.de
+ */
